b51ac7cf78e6e251d86466c5146a94bf585f3ed5
VisualSJ <itharbors@outlook.com>
2014-12-07 18:26:27 +0800

    游戏源代码

---
+++ template/src/resource.js
@@ -0,0 +1,23 @@
+
+//初始场景资源列表
+var dispatch_resources = [
+    "background.png",
+    "beerChildren.png",
+    "blood.png",
+    "gift.png",
+    "Deer.png",
+    "SantaClaus.png",
+    "guairen.png",
+    "lose.png",
+    "lvmaozi.png",
+    "Pole.png",
+    "ready.png",
+    "songshu.png",
+    "tree.png",
+    "tx.png",
+    "win.png",
+    "yw.png",
+
+    "jt1.png",
+    "sounds/ss.mp3"
+];

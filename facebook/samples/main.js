307fea0270cf1cb7562018f18b394783bed5a7a1
VisualSJ <itharbors@outlook.com>
2015-01-30 18:22:02 +0800

    UPDATE

--- facebook/samples/main.js
+++ facebook/samples/main.js
@@ -11,62 +11,59 @@ cc.game.onStart = function(){
             title: "login",
             callback: function(){
                 statusLabel.setString("... login ...");
-                facebook.login(function(errCode, result){
-                    console.log(result);
-                    if(errCode === 0){
-                        statusLabel.setString("login success, errCode: " + errCode);
-                    }else{
-                        statusLabel.setString("login failure, errCode: " + errCode);
-                    }
-                });
+
+
+                if (facebook.isLoggedIn()) {
+                    statusLabel.setString("logged in");
+                }
+                else {
+                    facebook.login(function (type, msg) {
+                        statusLabel.setString("type is " + type + " msg is " + JSON.stringify(msg));
+                    });
+                }
             }
         },{
             title: "logout",
             callback: function(){
                 statusLabel.setString("... logout ...");
-                facebook.logout(function(errCode, result){
-                    console.log(result);
-                    if(errCode === 0){
-                        statusLabel.setString("logout success, errCode: " + errCode);
-                    }else{
-                        statusLabel.setString("logout failure, errCode: " + errCode);
-                    }
+
+                facebook.logout(function (type, msg) {
+                    statusLabel.setString(JSON.stringify(msg));
                 });
             }
         },{
             title: "getUserId",
             callback: function(){
                 statusLabel.setString("... getUserId ...");
-                facebook.request("/me", plugin.FacebookAgent.HttpMethod.Get, {}, function(errCode, result){
-                    console.log(result);
-                    if(errCode === 0){
-                        statusLabel.setString("User id: " + result['id']);
-                    }else{
-                        statusLabel.setString("get user id failure");
-                    }
-                });
+
+
+                if (facebook.isLoggedIn()) {
+                    statusLabel.setString(facebook.getUserID());
+                }
+                else {
+                    statusLabel.setString("User haven't been logged in");
+                }
             }
         },{
             title: "getToken",
             callback: function(){
                 statusLabel.setString("... getToken ...");
-                facebook.requestAccessToken(function(errCode, result){
-                    console.log(result);
-                    if(errCode === 0){
-                        statusLabel.setString("token: " + result['accessToken'].substr(0, 15) + '...');
-                    }else{
-                        statusLabel.setString("get token failure");
-                    }
-                });
+
+                if (facebook.isLoggedIn()) {
+                    statusLabel.setString(facebook.getAccessToken());
+                }
+                else {
+                    statusLabel.setString("User haven't been logged in");
+                }
             }
         },{
-            title: "new permissions",
+            title: "loginWithPermissionClick",
             callback: function(){
                 statusLabel.setString("... new permissions ...");
                 autoExitFull && cc.screen.exitFullScreen();
                 var permissions = ["create_event", "create_note", "manage_pages", "publish_actions"];
-                facebook.requestPermissions(permissions, function(errCode, result){
-                    if(errCode === 0){
+                facebook.login(permissions, function(errCode, result){
+                    if(errCode === window["plugin"].FacebookAgent.CODE_SUCCEED){
                         statusLabel.setString("permissions: " + JSON.stringify(result['permissions']));
                     }else{
                         statusLabel.setString("requestPermissions failure");
@@ -74,12 +71,27 @@ cc.game.onStart = function(){
                 });
             }
         },{
-            title: "request api",
+            title: "getPermissions",
+            callback: function(){
+                facebook.api("/me/permissions", window["plugin"].FacebookAgent.HttpMethod.GET, {}, function (type, data) {
+                    if (type == window["plugin"].FacebookAgent.CODE_SUCCEED) {
+                        data = JSON.stringify(data);
+                        statusLabel.setString(data);
+                    }
+                    else {
+                        statusLabel.setString(JSON.stringify(data));
+                    }
+                });
+            }
+        },{
+            title: "request API",
             callback: function(){
                 statusLabel.setString("... request api ...");
-                facebook.request("/me/photos", plugin.FacebookAgent.HttpMethod.Post, {"url":"http://files.cocos2d-x.org/images/orgsite/logo.png"}, function(errCode, result){
-                    if(errCode === 0){
-                        statusLabel.setString("post_id: " + result["post_id"]);
+
+
+                facebook.api("/me/photos", window["plugin"].FacebookAgent.HttpMethod.POST, {"url": "http://files.cocos2d-x.org/images/orgsite/logo.png"}, function (type, msg) {
+                    if (type == window["plugin"].FacebookAgent.CODE_SUCCEED) {
+                        statusLabel.setString("post_id: " + msg["post_id"]);
                     }else{
                         statusLabel.setString("request failure");
                     }
@@ -88,24 +100,31 @@ cc.game.onStart = function(){
         },
         //share
         {
-            title: "Share link",
+            title: "share a simple link",
             callback: function(){
-                statusLabel.setString("... Share link ...");
-                autoExitFull && cc.screen.exitFullScreen();
+                statusLabel.setString("... share a simple link ...");
                 var map = {
-                    "dialog": "share_link",
-                    "description": "Cocos2d-x is a great game engine",
-                    "title": "Cocos2d-x",
-                    "link": "http://www.cocos2d-x.org",
-                    "imageUrl": "http://files.cocos2d-x.org/images/orgsite/logo.png"
+                    "dialog": "shareLink",
+                    "link": "http://www.cocos2d-x.org"
                 };
-                facebook.share(map, function (errCode, result) {
-                    if(errCode === 0){
-                        statusLabel.setString("result: " + JSON.stringify(result));
-                    }else{
-                        statusLabel.setString("share failure");
-                    }
-                });
+                if(facebook.canPresentDialog(map)){
+                    facebook.dialog(map,function(errorCode,msg){
+                        if(errorCode === window["plugin"].FacebookAgent.CODE_SUCCEED){
+                            statusLabel.setString(JSON.stringify(msg));
+                        }else{
+                            statusLabel.setString("share failure");
+                        }
+                    });
+                }else{
+                    map["dialog"] = "feedDialog";
+                    facebook.dialog(map,function(errorCode,msg){
+                        if(errorCode === window["plugin"].FacebookAgent.CODE_SUCCEED) {
+                            statusLabel.setString(JSON.stringify(msg));
+                        }else{
+                            statusLabel.setString("share failure");
+                        }
+                    });
+                }
             }
         },{
             title: "Share open graph",
@@ -209,107 +228,14 @@ cc.game.onStart = function(){
                     var menuItem = cc.MenuItemLabel.create(label, item['callback'], this);
                     menuItem.setColor(cc.color.BLACK);
 
-                    menuItem.x = (i / 6 | 0) ? 250 : -250;
-                    menuItem.y = ((i % 6) - 3) * -50;
+                    menuItem.x = (i / 7 | 0) ? 250 : -250;
+                    menuItem.y = ((i % 7) - 3) * -50;
 
                     menu.addChild(menuItem);
                 })(i);
             }
 
-
-            var full = cc.MenuItemLabel.create(
-                cc.LabelTTF.create("Full Screen", "Arial", 15),
-                function(){
-                    cc.screen.requestFullScreen();
-                }, this);
-            var zUp = cc.MenuItemLabel.create(
-                cc.LabelTTF.create("PzOrder9", "Arial", 15),
-                function(){
-                    window.top.document.getElementById("iframe_canvas").style.zIndex = 999999;
-                }, this);
-            zUp.x = 100;
-            var zDown = cc.MenuItemLabel.create(
-                cc.LabelTTF.create("PzOrder0", "Arial", 15),
-                function(){
-                    window.top.document.getElementById("iframe_canvas").style.zIndex = 0;
-                }, this);
-            zDown.x = 200;
-            var zDown2 = cc.MenuItemLabel.create(
-                cc.LabelTTF.create("PzOrder-9", "Arial", 15),
-                function(){
-                    window.top.document.getElementById("iframe_canvas").style.zIndex = -999999;
-                }, this);
-            zDown2.y = -40;
-            var cUp9 = cc.MenuItemLabel.create(
-                cc.LabelTTF.create("CzOrder9", "Arial", 15),
-                function(){
-                    document.body.style.zIndex = 999999;
-                }, this);
-            cUp9.y = -40;
-            cUp9.x = 100;
-            var cDown0 = cc.MenuItemLabel.create(
-                cc.LabelTTF.create("CzOrder0", "Arial", 15),
-                function(){
-                    document.body.style.zIndex = 0;
-                }, this);
-            cDown0.y = -40;
-            cDown0.x = 200;
-            var cDown2 = cc.MenuItemLabel.create(
-                cc.LabelTTF.create("CzOrder-9", "Arial", 15),
-                function(){
-                    document.body.style.zIndex = -999999;
-                }, this);
-            cDown2.y = -80;
-            var div = null;
-            var createDiv = cc.MenuItemLabel.create(
-                cc.LabelTTF.create("createDiv", "Arial", 15),
-                function(){
-                    if(div) return;
-                    div = document.createElement("div");
-                    div.style.width = "100px";
-                    div.style.height = "100px";
-                    div.style.position = "absolute";
-                    div.style.left = 100;
-                    div.style.top = 100;
-                    div.style.background = "black";
-
-                    document.body.appendChild(div);
-                }, this);
-            createDiv.y = -80;
-            createDiv.x = 100;
-            var removeDiv = cc.MenuItemLabel.create(
-                cc.LabelTTF.create("removeDiv", "Arial", 15),
-                function(){
-                    div && document.body.removeChild(div);
-                }, this);
-            removeDiv.y = -80;
-            removeDiv.x = 200;
-
-            //full screen
-            var FSmenu = new cc.Menu(full, zUp, zDown, zDown2, cUp9, cDown0, cDown2, createDiv, removeDiv);
-            FSmenu.setColor(cc.color.BLACK);
-            FSmenu.x = 550;
-            FSmenu.y = 100;
             this.addChild(menu);
-            this.addChild(FSmenu);
-
-            var al = cc.LabelTTF.create("Automatic exit full screen (Closed)", "Arial", 15);
-            var auto = cc.MenuItemLabel.create(
-                al,
-                function(){
-                    if(autoExitFull){
-                        al.setString("Automatic exit full screen (Closed)");
-                        autoExitFull = false;
-                    }else{
-                        al.setString("Automatic exit full screen (Opened)");
-                        autoExitFull = true;
-                    }
-                }, this);
-            var autoScreen = new cc.Menu(auto);
-            autoScreen.setColor(cc.color.BLACK);
-            autoScreen.x = 650;
-            autoScreen.y = 140;
-            this.addChild(autoScreen);
         }
     });
 
@@ -317,11 +243,6 @@ cc.game.onStart = function(){
     cc.LoaderScene.preload(["images/HelloWorld.png"], function () {
         cc.director.runScene(new MyScene());
     }, this);
-
-
-    //测试全屏后弹出部分窗口的问题所需要的参数测试
-    console.log(window.parent);
-    console.log(window.parent.document.getElementById("iframe_canvas").style.zIndex);
 
 };
 cc.game.run("gameCanvas");

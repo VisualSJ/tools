88f2d7300daf35fca00df5e70e5dcbb75bec54c3
VisualSJ <itharbors@outlook.com>
2015-02-02 15:50:22 +0800

    更新动态加密

--- controller/wechat/signature.js
+++ controller/wechat/signature.js
@@ -7,99 +7,60 @@ exports.plug = [
     {name: "param", handle: ["GET"]}
 ];
 
-exports.filter = null;
-
 var appid = "wx97722cd2036335d0";
 var secret = fs.readFileSync(path.join(__dirname, "./secret")) + "";
 
 var data;
 
-var tokenFile = path.join(__dirname, "./token");
-var token;
-if(fs.existsSync(tokenFile)){
-    token = fs.readFileSync(tokenFile) + "";
-}
-var timer_token = 0;
-
 var getToken = function(callback){
-    if(!token || new Date() - timer_token > 7200000){
-        timer_token = new Date();
-        https.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+appid+"&secret="+secret, function(res) {
+    https.get("https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+appid+"&secret="+secret, function(res) {
 
-            data = '';
-            res.on('data', function(chunk){
-                data += chunk;
-            });
-            res.on('end', function(){
-                data = data.toString();
-                try{
-                    token = JSON.parse(data).access_token;
-                }catch(err){
-                    token = null;
-                }
-
-                if(token){
-                    fs.writeFileSync(tokenFile, token);
-                    callback(token);
-                }else{
-                    callback("");
-                }
-            });
-
-        }).on('error', function(e) {
-            callback("");
+        data = '';
+        res.on('data', function(chunk){
+            data += chunk;
         });
-    }else{
-        if(token){
-            callback(token);
-        }else{
-            callback("");
-        }
-    }
+        res.on('end', function(){
+            data = data.toString();
+            try{
+                callback(JSON.parse(data).access_token);
+            }catch(err){
+                callback("");
+            }
+        });
+
+    }).on('error', function(e) {
+        callback("");
+    });
 };
 
-var ticketFile = path.join(__dirname, "./ticket");
-var ticket;
-if(fs.existsSync(ticketFile)){
-    ticket = fs.readFileSync(ticketFile) + "";
-}
-var timer_ticket = 0;
-
 var getTicket = function(token, callback){
-    if(!ticket || new Date() - timer_ticket > 7200000){
-        timer_ticket = new Date();
-        https.get("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token="+token+"&type=jsapi", function(res) {
+    https.get("https://api.weixin.qq.com/cgi-bin/ticket/getticket?access_token="+token+"&type=jsapi", function(res) {
 
-            data = '';
-            res.on('data', function(chunk){
-                data += chunk;
-            });
-            res.on('end', function(){
-                data = data.toString();
-                try{
-                    ticket = JSON.parse(data).ticket;
-                }catch(err){
-                    ticket = null;
-                }
-
-                if(ticket){
-                    fs.writeFileSync(ticketFile, ticket);
-                    callback(ticket);
-                }else{
-                    callback("");
-                }
-            });
-
-        }).on('error', function(e) {
-            callback("");
+        data = '';
+        res.on('data', function(chunk){
+            data += chunk;
         });
-    }else{
-        if(ticket){
-            callback(ticket);
-        }else{
-            callback("");
-        }
+        res.on('end', function(){
+            data = data.toString();
+            try{
+                callback(JSON.parse(data).ticket);
+            }catch(err){
+                callback("");
+            }
+        });
+
+    }).on('error', function(e) {
+        callback("");
+    });
+};
+
+var getNoncestr = function(){
+    var result = "";
+    var txt = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    for(var i= 0, len=(Math.random()*20)|0; i<len; i++){
+        result += txt[(Math.random()-0.1)*63 | 0];
     }
+    return result;
 };
 
 exports.filter = function(request, response, method, next){
@@ -112,29 +73,46 @@ exports.filter = function(request, response, method, next){
 
 exports.handle = function(request, response, method){
 
-    getToken(function(token){
-        getTicket(token, function(ticket){
-            var url = method.param.GET("url");
-            var noncestr = "VQhYOUJRz6RolHqN";
-            var stamp = (new Date() - 0) / 1000 | 0;
+    var data = fs.readFileSync(path.join(__dirname, "./state.json")).toString();
+    data = JSON.parse(data);
 
-            var content = "jsapi_ticket=" + ticket + "&" +
-                "noncestr=" + noncestr + "&" +
-                "timestamp=" + stamp + "&" +
-                "url=" + url;
+    var current = new Date() - 0;
 
-            var shasum = crypto.createHash('sha1');
+    if(data.deadline < current){
+        console.log("update weChat secret" + (new Date() - 0));
+        getToken(function(token){
+            getTicket(token, function(ticket){
+                fs.writeFileSync(path.join(__dirname, "./state.json"), JSON.stringify({
+                    deadline: current + 7200000,
+                    token: token,
+                    ticket: ticket
+                }));
 
-            shasum.update(content);
-
-            var result = shasum.digest('hex');
-
-            response.end(JSON.stringify({
-                url: url,
-                noncestr: noncestr,
-                stamp: stamp,
-                signature: result
-            }));
+                exports.handle(request, response, method);
+            });
         });
-    });
+
+    }else{
+        var url = method.param.GET("url");
+        var noncestr = getNoncestr();
+        var stamp = (new Date() - 0) / 1000 | 0;
+
+        var content = "jsapi_ticket=" + data.ticket + "&" +
+            "noncestr=" + noncestr + "&" +
+            "timestamp=" + stamp + "&" +
+            "url=" + url;
+
+        var shasum = crypto.createHash('sha1');
+
+        shasum.update(content);
+
+        var result = shasum.digest('hex');
+
+        response.end(JSON.stringify({
+            url: url,
+            noncestr: noncestr,
+            stamp: stamp,
+            signature: result
+        }));
+    }
 };
